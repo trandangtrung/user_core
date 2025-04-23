aaa4a253c3a52b8bdea10f6cd394a93e6b8f3508
trandangtrung <trantrung210103@gmail.com>
2025-04-23 22:46:40 +0700

    complete service

--- internal/entity/user.go
+++ internal/entity/user.go
@@ -6,6 +6,9 @@ import (
 
 type User struct {
 	gorm.Model
+	Name           string `json:"name" gorm:"type:varchar(255);not null"`
 	Email          string `json:"email" gorm:"type:varchar(255);not null;unique"`
 	PasswordHashed string `json:"password_hashed" gorm:"type:varchar(255);not null"`
+	CreatedBy      uint   `json:"created_by" gorm:"not null"`
+	UpdatedBy      uint   `json:"updated_by" gorm:"not null"`
 }
--- internal/service/user-role.go
+++ internal/service/user-role.go
@@ -3,7 +3,10 @@ package service
 import (
 	"context"
 	v1 "demo/api/userRole/v1"
+	"demo/internal/entity"
 	"demo/internal/repository"
+
+	"github.com/gogf/gf/v2/os/gtime"
 )
 
 type (
@@ -25,17 +28,73 @@ func NewUserRoleService(userRoleRepo repository.UserRoleRepository) UserRoleServ
 }
 
 func (u *userRoleService) Get(ctx context.Context, req *v1.GetReq) (res *v1.GetRes, err error) {
-	return nil, nil
+	userRole, err := u.userRoleRepo.GetById(ctx, int(req.Id))
+	if err != nil {
+		return nil, err
+	}
+
+	// Trả về kết quả
+	return &v1.GetRes{
+		Id:       int64(userRole.ID),
+		UserId:   int64(userRole.UserID),
+		RoleId:   int64(userRole.RoleID),
+		CreateAt: gtime.New(userRole.CreatedAt),
+		CreateBy: int64(userRole.CreatedBy),
+		UpdateAt: gtime.New(userRole.UpdatedAt),
+	}, nil
 }
 
 func (u *userRoleService) Create(ctx context.Context, req *v1.CreateReq) (res *v1.CreateRes, err error) {
-	return nil, nil
+	userRole := &entity.UserRole{
+		UserID: uint(req.UserId),
+		RoleID: uint(req.RoleId),
+	}
+
+	// Gọi repository để tạo mới UserRole
+	created, err := u.userRoleRepo.Create(ctx, userRole)
+	if err != nil {
+		return nil, err
+	}
+
+	// Trả về response
+	return &v1.CreateRes{
+		Id:       int64(created.ID),
+		UserId:   int64(created.UserID),
+		RoleId:   int64(created.RoleID),
+		CreateAt: gtime.New(created.CreatedAt),
+		CreateBy: int64(created.CreatedBy),
+	}, nil
 }
 
 func (u *userRoleService) Update(ctx context.Context, req *v1.UpdateReq) (res *v1.UpdateRes, err error) {
-	return nil, nil
+	userRole, err := u.userRoleRepo.GetById(ctx, int(req.Id))
+	if err != nil {
+		return nil, err
+	}
+
+	userRole.UserID = uint(req.UserId)
+	userRole.RoleID = uint(req.RoleId)
+
+	// Lưu lại các thay đổi vào DB
+	updatedUserRole, err := u.userRoleRepo.Update(ctx, userRole)
+	if err != nil {
+		return nil, err
+	}
+
+	// Trả về response
+	return &v1.UpdateRes{
+		Id:       int64(updatedUserRole.ID),
+		UserId:   int64(updatedUserRole.UserID),
+		RoleId:   int64(updatedUserRole.RoleID),
+		UpdateAt: gtime.New(updatedUserRole.UpdatedAt),
+		UpdateBy: int64(updatedUserRole.UpdatedBy),
+	}, nil
 }
 
 func (u *userRoleService) Delete(ctx context.Context, id int64) error {
+	err := u.userRoleRepo.Delete(ctx, int(id))
+	if err != nil {
+		return err
+	}
 	return nil
 }
